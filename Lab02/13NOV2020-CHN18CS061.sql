ffa972190eb6f99eb520194f0e2c0ae23fdd9047
jesswinw-061 <74049279+jesswinw-061@users.noreply.github.com>
2020-11-13 13:08:36 +0530

    Add files via upload

---
+++ Lab02/13NOV2020-CHN18CS061.sql
@@ -0,0 +1,31 @@
+#1
+CREATE TABLE EMPLOYEE
+( code CHAR(4),
+name VARCHAR(20),
+designation VARCHAR(30),
+dob DATE,
+salary  FLOAT);
+
+
+#2
+INSERT INTO EMPLOYEE (code , name , designation , dob , salary)
+VALUES
+    ('e1' ,'Gon Feress' ,'Manager' ,'1992-09-05',25000),
+    ('e2','Ash' ,'Asst Manager','1995-08-01',24500) ;
+       
+#3
+SELECT * FROM EMPLOYEE;
+
+#4
+UPDATE EMPLOYEE
+SET
+salary =30000
+WHERE 
+code ='e1';
+
+SELECT * FROM EMPLOYEE;
+
+#5
+DELETE FROM EMPLOYEE;
+
+SELECT * FROM EMPLOYEE;
