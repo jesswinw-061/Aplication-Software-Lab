d890aa558f18d418982a9e37aca633afaae80ab6
jesswinw-061 <74049279+jesswinw-061@users.noreply.github.com>
2021-01-08 14:24:12 +0530

    Update main.sql

--- Lab08/main.sql
+++ Lab08/main.sql
@@ -1,29 +1,31 @@
-#a
-CREATE TABLE store(
-	    order_no int primary key AUTO_INCREMENT,
-	    code varchar(10) not null,
-	    item varchar(30) not null,
-	    quantity int default 0,
-	    price float,
-	    discount int default 0,
-	    mrp float not null
+#1 
+CREATE TABLE Employee(
+    code CHAR(6) PRIMARY KEY,
+    name VARCHAR(80),
+    dob date,
+    designation VARCHAR(100),
+    salary FLOAT
 );
 
-#b
-INSERT INTO store(code,item,quantity,price,discount,mrp)
-VALUES 
-("a123", "Macbook Pro 13", 14, 100999.99, 2000, 120000),
-("a420", "iPhone 12", 12, 90000, 1000, 119900),
-("a123", "Airpods Pro", 10, 30000, 0, 59900);
+#2
+INSERT INTO Employee (code, name, dob, designation, salary)
+VALUES
+('gm0121','Emmanuel Shine Joseph','2000-10-05','General Manager',50000.25),
+('pm2918','Arjun G','1998-11-15','Product Manager',50000.50),
+('cl0123','Kumar','1992-02-06','Clerk',25000),
+('cl1234','Ram','2001-02-05','Clerk',15000.75);
 
-#c
-SELECT * FROM store; 
+#3
+SELECT SUM(salary) FROM Employee WHERE designation='Clerk';
 
-#d
-SELECT MOD(price,9) FROM store;
+#4
+SELECT MAX(salary) FROM Employee;
 
-#e
-SELECT price,POWER(price,2) FROM store;
+#5
+SELECT AVG(salary) FROM Employee;
 
-#f
-SELECT ROUND(mrp DIV 7) FROM store;
+#6
+SELECT MIN(salary) FROM Employee;
+
+#7
+SELECT COUNT(*) FROM Employee;
